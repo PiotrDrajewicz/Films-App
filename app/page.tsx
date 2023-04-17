35a25cd9efc5e6246a90452bb74f8e6277b9ff68
unknown <drpiterek@gmail.com>
2023-04-18 00:37:24 +0200

    setting next 3

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>Home page</h1>
+            <h3>This is content</h3>
+        </div>
+    )
+}
+
+export default HomePage;
