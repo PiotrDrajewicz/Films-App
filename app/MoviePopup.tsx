4b67cb8f22a6c7c335c7195c6367acce610d6227
unknown <drpiterek@gmail.com>
2023-04-23 23:36:19 +0200

    Done closing popup and digits for rating. Need to do this for wider screens

--- app/MoviePopup.tsx
+++ app/MoviePopup.tsx
@@ -1,21 +1,42 @@
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faStarHalfStroke, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 interface MoviePopupInterface {
     title: string;
     poster_path: string;
     setIsActive: (isOpen: boolean) => void;
 }
+const isOpen: boolean = false;
 
 const MoviePopup: React.FC<MoviePopupInterface> = ({title, poster_path, setIsActive}) => {
+    const [isFav, setIsFav] = useState<boolean>(false);
+    const [isRateOpen, setIsRateOpen] = useState<boolean>(false);
+
+    const toggleFav = (): void => {
+        setIsFav(prev => !prev);
+    }
+
+    const toggleRate = (): void => {
+        setIsRateOpen(prev => !prev);
+    }
 
     return (
         <section className="movie-popup-container">
             <div className="movie-popup">
                 <img className="movie-poster-popup" src={`https://image.tmdb.org/t/p/original${poster_path}`} alt="movie poster popup" />
                 <div className="movie-popup-buttons">
-                    <FontAwesomeIcon className="popup-icon add-fav-icon" icon={faHeart} onClick={() => setIsActive} style={{color: 'white'}} size='2x' />
-                    <FontAwesomeIcon className="popup-icon rate-icon" icon={faStarHalfStroke} style={{color: 'white'}} size='2x' />
+                    <FontAwesomeIcon className="popup-icon add-fav-icon" icon={faHeart} style={ isFav ? {color: 'red'} : {color: 'white'}} onClick={toggleFav} size='2x' />
+                    <FontAwesomeIcon className="popup-icon rate-icon" icon={faStarHalfStroke} style={{color: 'white'}} onClick={toggleRate} size='2x' />
+                    <div className={`rate-numbers ${ isRateOpen ? 'open' : null}`}>
+                        <p className={`rate-number ${ isRateOpen ? 'visible' : null}`}>0</p>
+                        <p className={`rate-number ${ isRateOpen ? 'visible' : null}`}>1</p>
+                        <p className={`rate-number ${ isRateOpen ? 'visible' : null}`}>2</p>
+                        <p className={`rate-number ${ isRateOpen ? 'visible' : null}`}>3</p>
+                        <p className={`rate-number ${ isRateOpen ? 'visible' : null}`}>4</p>
+                        <p className={`rate-number ${ isRateOpen ? 'visible' : null}`}>5</p>
+                    </div>
+                    <FontAwesomeIcon className='close-icon' icon={faXmark} onClick={() => setIsActive(isOpen)} style={{color: 'white'}} size='2x' />
                 </div>
             </div>
         </section>
