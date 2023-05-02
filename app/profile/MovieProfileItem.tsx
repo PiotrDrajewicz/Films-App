af2b9a182469c6f3355632f2f9679815782a74f2
unknown <drpiterek@gmail.com>
2023-05-02 12:32:06 +0200

    I've done DELETE method on profile.

--- app/profile/MovieProfileItem.tsx
+++ app/profile/MovieProfileItem.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import Link from "next/link";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeartCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { useRouter } from 'next/navigation';
 
 interface MovieProfileItem {
     id: string;
@@ -13,15 +16,27 @@ interface MovieProfileItem {
 
 const MovieProfileItem: React.FC<MovieProfileItem> = ({id, movieId, title, poster_path, overview, collectionName}) => {
 
+    const router = useRouter();
+
+    const deleteFav = async () => {
+        await fetch(`http://127.0.0.1:8090/api/collections/fav_movies/records/${id}`, {
+            method: 'DELETE',
+        });
+
+        router.refresh();
+    }
+
     return (
         <div className="movie-profile-container">
-            <Link href={`/profile/${id}`} className="movie-profile-link">
-                <article className="movie-arr-item">
-                    <img src={`https://image.tmdb.org/t/p/original${poster_path}`} className='profile-poster movie-info-item' alt="movie image in profile" />
-                    <h3 className="movie-info-item profile-movie-title">{title}</h3>
-                    <FontAwesomeIcon className="delete-fav-icon movie-info-item" icon={faHeartCircleXmark} size='2x' />
-                </article>
-            </Link>
+            <article className="movie-arr-item">
+                <Link href={`/profile/${id}`} className="movie-profile-link" onClick={() => console.log('list item')}>
+                    <div className="link-container">
+                        <img src={`https://image.tmdb.org/t/p/original${poster_path}`} className='profile-poster movie-info-item' alt="movie image in profile" />
+                        <h3 className="movie-info-item profile-movie-title">{title}</h3>
+                    </div>
+                </Link>
+                    <FontAwesomeIcon className="delete-fav-icon movie-info-item" icon={faHeartCircleXmark} size='2x' onClick={deleteFav} />
+            </article>
         </div>
     )
 }
