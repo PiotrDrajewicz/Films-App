33e864297ca4301caec593197b87f939135520eb
unknown <drpiterek@gmail.com>
2023-05-03 11:52:50 +0200

    working on toggling lists

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,7 @@
-'use client'
+'use client';
 
 import MovieProfileItem from "./MovieProfileItem";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const getMovies = async () => {
     const res = await fetch('http://127.0.0.1:8090/api/collections/fav_movies/records?page=1&perPage=30', 
@@ -12,17 +12,26 @@ const getMovies = async () => {
 }
 
 const ProfilePage = async () => {
-    const [isFavOpen, setIsFavOpen] = useState<boolean>(true);
-    const favMovies = await getMovies();
+    // const [isFavOpen, setIsFavOpen] = useState<boolean>(true);
+    const titleRef = useRef(null);
     
+    const favMovies = await getMovies();
+    console.log('re-render');
+    
+    const toggleFav = () => {
+        // setIsFavOpen(prev => !prev);
+        // console.log('triggered');
+        console.log(titleRef.current);
+    }
+
     return (
         <>
             <div className="profile-flex-container">
                 <div className="all-movies-container">
                     <section className="fav-movies-section">
                         <div className="profile-title-container">
-                            <h1 className={`profile-title`} >Favourite movies</h1>
-                            <h1 className={`profile-title`} >Rated movies</h1>
+                            <h1 className={`profile-title`}  >Favourite movies</h1>
+                            <h2 className={`profile-title`} onClick={toggleFav}>Rated movies</h2>
                         </div>
                         <div className="fav-movies-container">
                             {favMovies?.map((movie) => {
